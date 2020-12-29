64f9c1b9c663d1ff3ce6f6ba399c99fe6864eb3d
devops056 <64005717+devops056@users.noreply.github.com>
2020-12-29 11:23:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
+'use strict';
+
 const express = require('express');
-const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const router = express.Router();
 
-// Middlewares
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// API
-app.use('/', require('./controller/controller'));
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 // App
 const app = express();
@@ -17,8 +12,5 @@ app.get('/', (req, res) => {
   res.send('<h1 style="color:green;">Java Home App - version-11!!</h1> \n');
 });
 
-// Server
-const port = process.env.PORT;
-app.listen(port, function () {
-    console.log('listening on port:' + port);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
